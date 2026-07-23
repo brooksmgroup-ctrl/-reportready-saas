b72c1d516332498f96945fa6721270396a81ee6e
ReportReady Engineer <engineer@getreportready.com>
2026-07-23 15:48:14 +0000

    Fix broken from address syntax in campaignManager.js

--- server/campaignManager.js
+++ server/campaignManager.js
@@ -77,7 +77,7 @@ async function sendEmail(email, { subject, text }) {
   }
   try {
     const { data, error } = await resend.emails.send({
-   hello@getreportready.com    ,
+   from: 'ReportReady <hello@getreportready.com>',
       to: [email], subject, text
     });
     if (error) throw error;
